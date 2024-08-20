599bb2ee0a4a4bfc00d43d550e4629086d73579d
Cedric Ziel <cedric@cedric-ziel.com>
2024-08-20 15:26:01 -0600

    fine-tune loadgen

--- load/script.js
+++ load/script.js
@@ -3,6 +3,7 @@ import { check, sleep } from 'k6';
 import { browser } from 'k6/experimental/browser';
 
 export const options = {
+    vus: 2,
     scenarios: {
         ui: {
             executor: 'shared-iterations',
@@ -23,43 +24,28 @@ export default async function () {
     const context = browser.newContext();
     const page = context.newPage();
 
-    http.get(`http://frontend:8000`);
+    http.get(`http://frontproxy:8000`);
     sleep(1);
 
     try {
-        await page.goto(`http://frontend:8000`);
+        await page.goto(`http://frontproxy:8000`);
         sleep(1);
 
-        const productsNavItem = page.locator("//a[starts-with(text(),'Products')]")
+        const productsNavItem = page.locator("//a[starts-with(text(),'Products')]");
         await Promise.all([page.waitForNavigation(), productsNavItem.click()]);
 
         const productToFind = Math.floor(Math.random() * 9) + 1;
 
-        const detailPageButton = page.locator(`#product-list div.product:nth-child(${productToFind}) a.btn`);
+        const productButton = page.locator(`.products button.product:nth-child(${productToFind})`);
+        productButton.click();
 
-        await Promise.all([page.waitForNavigation(), detailPageButton.click()]);
+        const reviewOrderButton = page.locator("button.placeOrder");
+        await Promise.all([page.waitForNavigation(), reviewOrderButton.click()]);
+
+        const placeOrderButton = page.locator("button.placeOrder");
+        await Promise.all([page.waitForNavigation(), placeOrderButton.click()]);
+
         sleep(1);
-
-        check(page, {
-            'header': p => p.locator('h1').textContent() == 'Faros',
-        });
-
-        // Product detail page
-        page.locator('input[name="add_to_cart[quantity]"]').type(1);
-        const addButton = page.locator('button#add_to_cart_add')
-
-        await Promise.all([page.waitForNavigation(), addButton.click()]);
-        sleep(1);
-
-        // checkout
-        const checkoutButton = page.locator('a#checkout');
-
-        await Promise.all([page.waitForNavigation(), checkoutButton.click()]);
-        sleep(1);
-
-        check(page, {
-            'header': p => p.locator('h1').textContent() == 'Checkout Succeeded!',
-        });
 
         page.waitForTimeout(5000);
     } finally {
