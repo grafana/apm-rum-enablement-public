985546cf5a6b30e58ce2b4c8912a99b15fb958a3
Cedric Ziel <cedric@cedric-ziel.com>
2024-08-22 13:15:51 -0600

    changes

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,21 @@
 const { trace }  = require("@opentelemetry/api");
 
-module.exports = function (app) {
-    app.use(function (req, res, next) {
-      // set traceparent header
-      const traceId = trace.getActiveSpan()?.spanContext()?.traceId ?? '00000';
-      const spanId = trace.getActiveSpan()?.spanContext()?.spanId ?? '00000';
+function nonBlockingDelay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-      res.setHeader("server-timing", `traceparent;desc="00-${traceId}-${spanId}-01"`);
-      next();
+module.exports = function (app) {
+    app.use(async function (req, res, next) {
+        // set traceparent header
+        const traceId = trace.getActiveSpan()?.spanContext()?.traceId ?? '00000';
+        const spanId = trace.getActiveSpan()?.spanContext()?.spanId ?? '00000';
+
+        res.setHeader("server-timing", `traceparent;desc="00-${traceId}-${spanId}-01"`);
+
+        // sleep randomly
+        let delay = Math.floor(Math.random() * (1900 - 60 + 1));
+        await nonBlockingDelay(delay);
+
+        next();
     });
 };
